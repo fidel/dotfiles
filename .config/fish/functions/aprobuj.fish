9186b59e7d9fd3b45361d1420062fcde6be8d8ff
Szymon Fiedler <szymon.fiedler@gmail.com>
2025-02-18 16:23:07 +0100

    Valid search parameters

--- .config/fish/functions/aprobuj.fish
+++ .config/fish/functions/aprobuj.fish
@@ -6,6 +6,6 @@ function aprobuj
     else
         set author $argv[1]
         set since (test (count $argv) -gt 1; and echo $argv[2]; or date +%Y-%m-%d)
-        gh pr list --author $author --search "created:>$since" -S review:none --json url -q ".[] | .url" | xargs -n1 gh pr review --approve
+        gh pr list --author $author --search "created:>=$since review:none" --json url -q ".[] | .url" | xargs -n1 gh pr review --approve
     end
 end

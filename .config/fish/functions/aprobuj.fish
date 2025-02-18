29090b62d07d20a40f03f2c884a487767fb655a6
Szymon Fiedler <szymon.fiedler@gmail.com>
2025-02-18 12:49:47 +0100

    Approve all given author's PRs or a specific PR by number

--- .config/fish/functions/aprobuj.fish
+++ .config/fish/functions/aprobuj.fish
@@ -1,3 +1,7 @@
 function aprobuj
-    gh pr list --author $argv -S review:none --json url -q ".[] | .url" | xargs -n1 gh pr review --approve
+    if test (string match -r '^\d+$' "$argv[1]")
+        gh pr review $argv[1] --approve
+    else
+        gh pr list --author $argv -S review:none --json url -q ".[] | .url" | xargs -n1 gh pr review --approve
+    end
 end

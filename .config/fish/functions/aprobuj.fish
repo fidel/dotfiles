afa1266ca1b198c7fb28490bc8957016877076d7
Szymon Fiedler <szymon.fiedler@gmail.com>
2025-02-18 12:52:53 +0100

    Approve given user PRs but created after a certain date
    So no old, leftover PRs are approved.

--- .config/fish/functions/aprobuj.fish
+++ .config/fish/functions/aprobuj.fish
@@ -4,6 +4,8 @@ function aprobuj
             gh pr review $pr --approve
         end
     else
-        gh pr list --author $argv -S review:none --json url -q ".[] | .url" | xargs -n1 gh pr review --approve
+        set author $argv[1]
+        set since $argv[2]
+        gh pr list --author $author --search "created:>$since" -S review:none --json url -q ".[] | .url" | xargs -n1 gh pr review --approve
     end
 end

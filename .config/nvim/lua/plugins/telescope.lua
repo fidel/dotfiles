a17fce9e2228506f0fba01a5a7d0550a24df0637
Szymon Fiedler <szymon.fiedler@gmail.com>
2025-10-24 16:07:26 +0200

    telescope.lua tweaks
    - Consolidated duplicate keymaps: Merged duplicate keymap definitions into a cleaner structure
    - Better organization: Added section comments, made helper function local instead of global
    - Consistency: All keymaps now use the same style and include proper descriptions

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -43,54 +43,46 @@ return {
       telescope.load_extension("fzf")
       telescope.load_extension("ui-select")
 
-      vim.keymap.set("n", "<leader>pf", function()
-        require("telescope.builtin").find_files({ previewer = false })
-      end, { desc = "Find files" })
-      vim.keymap.set("n", "<leader>p", function()
-        require("telescope.builtin").git_files({ previewer = false })
-      end, { desc = "Find files" })
-      vim.keymap.set("n", "<leader>e", function()
-        require("telescope.builtin").oldfiles()
-      end, { desc = "Old files" })
-      vim.keymap.set("n", "<leader>fg", function()
-        require("telescope.builtin").live_grep()
-      end, { desc = "Live grep" })
-      vim.keymap.set("n", "<leader>fb", function()
-        require("telescope.builtin").buffers({ sort_lastused = true })
-      end, { desc = "Buffers" })
+      local tb = require('telescope.builtin')
 
-      function vim.getVisualSelection()
+      -- Helper function to get visual selection
+      local function get_visual_selection()
         local current_clipboard_content = vim.fn.getreg('"')
-
         vim.cmd('noau normal! "vy"')
         local text = vim.fn.getreg('v')
         vim.fn.setreg('v', {})
-
         vim.fn.setreg('"', current_clipboard_content)
-
         text = string.gsub(text, "\n", "")
-        if #text > 0 then
-          return text
-        else
-          return ''
-        end
+        return #text > 0 and text or ''
       end
 
-      local keymap = vim.keymap.set
-      local tb = require('telescope.builtin')
-      local opts = { noremap = true, silent = true }
+      -- File navigation
+      vim.keymap.set("n", "<leader>pf", function()
+        tb.find_files({ previewer = false })
+      end, { desc = "Find files" })
 
-      keymap('n', '<leader>g', ':Telescope current_buffer_fuzzy_find<cr>', opts)
-      keymap('v', '<leader>g', function()
-        local text = vim.getVisualSelection()
-        tb.current_buffer_fuzzy_find({ default_text = text })
-      end, opts)
+      vim.keymap.set("n", "<leader>p", function()
+        tb.git_files({ previewer = false })
+      end, { desc = "Git files" })
 
-      keymap('n', '<leader>G', ':Telescope live_grep<cr>', opts)
-      keymap('v', '<leader>G', function()
-        local text = vim.getVisualSelection()
-        tb.grep_string({ search = text })
-      end, opts)
+      vim.keymap.set("n", "<leader>e", tb.oldfiles, { desc = "Old files" })
+
+      vim.keymap.set("n", "<leader>fb", function()
+        tb.buffers({ sort_lastused = true })
+      end, { desc = "Buffers" })
+
+      -- Search in current buffer
+      vim.keymap.set('n', '<leader>g', tb.current_buffer_fuzzy_find, { desc = "Search in buffer" })
+      vim.keymap.set('v', '<leader>g', function()
+        tb.current_buffer_fuzzy_find({ default_text = get_visual_selection() })
+      end, { desc = "Search selection in buffer" })
+
+      -- Live grep / grep string
+      vim.keymap.set('n', '<leader>fg', tb.live_grep, { desc = "Live grep" })
+      vim.keymap.set('n', '<leader>G', tb.live_grep, { desc = "Live grep" })
+      vim.keymap.set('v', '<leader>G', function()
+        tb.grep_string({ search = get_visual_selection() })
+      end, { desc = "Grep selection" })
     end,
   },
 }

f5cee9ffde844635d61599c4e65473fb2d40a5f0
Szymon Fiedler <szymon.fiedler@gmail.com>
2025-11-03 13:17:36 +0100

    Keybindings for nvim-tes

--- .config/nvim/lua/plugins/test.lua
+++ .config/nvim/lua/plugins/test.lua
@@ -3,10 +3,39 @@ return {
     "klen/nvim-test",
     config = function()
       require("nvim-test").setup({
+        termOpts = {
+          go_back = true,
+          direction = "horizontal",
+        },
         runners = {
           ruby = "nvim-test.runners.rspec",
         },
       })
+
+      require("nvim-test.runners.rspec"):setup({
+        command = "bundle",
+      })
+
+      vim.keymap.set(
+        "n",
+        "<Leader>tf",
+        "<Cmd>TestFile<CR>",
+        { desc = "Test opened file" }
+      )
+
+      vim.keymap.set(
+        "n",
+        "<Leader>tl",
+        "<Cmd>TestLast<CR>",
+        { desc = "Test opened file" }
+      )
+
+      vim.keymap.set(
+        "n",
+        "<Leader>tn",
+        "<Cmd>TestNearest<CR>",
+        { desc = "Test opened file" }
+      )
     end,
   },
 }
